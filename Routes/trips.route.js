0b2f3ecaf86a599175866137124917ae1b5c4fc2
MinaSamirSaad <minasamir.nashid@gmail.com>
2024-03-02 22:31:53 +0200

    update booking trips

--- Routes/trips.route.js
+++ Routes/trips.route.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const TripsController = require('../Controllers/trip.controller');
+const { auth } = require('../Middlewares/auth');
 
 // GET Methods
 router.get('/',TripsController.allTrips);
 router.get('/:id',TripsController.tripData);
-router.get('/book/:id',TripsController.bookTrip);
+router.get('/book/:id',auth,TripsController.bookTrip);
 
 // Post Methods
 router.post('/',TripsController.addTrip);
