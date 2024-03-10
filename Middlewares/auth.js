d64714708f7d4107aa3d84b871139886ec36b66e
MinaSamirSaad <minasamir.nashid@gmail.com>
2024-03-10 22:27:02 +0200

    update auth problem

--- Middlewares/auth.js
+++ Middlewares/auth.js
@@ -1,17 +1,17 @@
 const jwt = require('jsonwebtoken');
 const userModel = require('../Models/user.model');
-const resData = require('../helperFunctions');
+const { resData } = require('../helperFunctions');
 const auth = async (req, res, next) => {
     try {
-            const token = req.header("Authorization").replace("Bearer ", "");
-            const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            const userData = await userModel.findOne({
-                _id: decoded._id,
-                "tokens.token": token
-            })
-            if (!userData) throw new Error("Invalid token")
-            req.token = token
-            req.user = userData
+        const token = req.header("Authorization").replace("Bearer ", "");
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const userData = await userModel.findOne({
+            _id: decoded._id,
+            "tokens.token": token
+        })
+        if (!userData) throw new Error("Invalid token")
+        req.token = token
+        req.user = userData
         next()
     }
     catch (e) {
